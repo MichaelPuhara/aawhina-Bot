c5ac829e28a33275942044f6c23d48e3287417fa
MichaelPuhara <121073103+MichaelPuhara@users.noreply.github.com>
2023-01-11 20:16:31 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import gradio as gr
+import streamlit as st
 
 #if you have OpenAI API key as an environment variable, enable the below
 #openai.api_key = os.getenv("OPENAI_API_KEY")
